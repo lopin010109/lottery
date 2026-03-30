a9d0e23f356e77a1a1af88d5f07c6f7a01a23e28
Joshhsieh <halu_9@hotmail.com>
2026-03-30 12:17:19 +0800

    feat: update vesion add index version to check version is update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lottery-v1'; // 若有版本要更新重新部署需要改變版本，否則PWA會拿到舊版
+const CACHE = 'lottery-v1.1'; // 若有版本要更新重新部署需要改變版本，否則PWA會拿到舊版
 const FILES = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
@@ -11,12 +11,12 @@ self.addEventListener('activate', (e) => {
     caches
       .keys()
       .then((keys) =>
-        Promise.all(
-          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
-        ),
-      ),
+        Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+      )
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window', includeUncontrolled: true }))
+      .then((clients) => clients.forEach((client) => client.navigate(client.url)))
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
