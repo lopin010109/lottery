6f68d1bda8e46ac7c0ad3d5d3c80a387a01387af
Joshhsieh <halu_9@hotmail.com>
2026-03-30 11:58:54 +0800

    feat: add PWA and reivew code

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'lottery-v1'; // 若有版本要更新重新部署需要改變版本，否則PWA會拿到舊版
+const FILES = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
+        ),
+      ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request)),
+  );
+});
