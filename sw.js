727dfd4c9b38799cf6b6143037fb8f6660b7d137
Joshhsieh <halu_9@hotmail.com>
2026-03-30 12:17:34 +0800

    feat: update vesion add index version to check version is update

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
 const CACHE = 'lottery-v1.1'; // 若有版本要更新重新部署需要改變版本，否則PWA會拿到舊版
-const FILES = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES)));
@@ -11,11 +17,17 @@ self.addEventListener('activate', (e) => {
     caches
       .keys()
       .then((keys) =>
-        Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+        Promise.all(
+          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
+        ),
       )
       .then(() => self.clients.claim())
-      .then(() => self.clients.matchAll({ type: 'window', includeUncontrolled: true }))
-      .then((clients) => clients.forEach((client) => client.navigate(client.url)))
+      .then(() =>
+        self.clients.matchAll({ type: 'window', includeUncontrolled: true }),
+      )
+      .then((clients) =>
+        clients.forEach((client) => client.navigate(client.url)),
+      ),
   );
 });
 
